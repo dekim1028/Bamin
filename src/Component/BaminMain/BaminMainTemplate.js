d3f4208c15aaabbeaa43def1f96f293350af7936
dekim1028 <daeun235194@gmail.com>
2020-08-31 16:05:14 +0900

    Top 메뉴 개발

---
+++ src/Component/BaminMain/BaminMainTemplate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BaminTop from './BaminTop';
+import '../../Style/css/BaminMainTemplate.scss';
+
+const BaminMainTemplate = () => {
+    return (
+        <div className="BaminMainTemplate">
+            <BaminTop/>
+        </div>
+    );
+};
+
+export default BaminMainTemplate;
---
+++ src/Component/BaminMain/BaminTop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {MdNotificationsNone} from 'react-icons/md';
+import {AiOutlineQrcode, AiFillDownCircle} from 'react-icons/ai';
+import '../../Style/css/BaminTop.scss';
+
+const BaminTop = () => {
+    return (
+        <div className="BaminTop">
+            <MdNotificationsNone/>
+            <span className="addressText">서울 동작구 사당로 16다길 101 <AiFillDownCircle/></span>
+            <AiOutlineQrcode/>
+        </div>
+    );
+};
+
+export default BaminTop;
