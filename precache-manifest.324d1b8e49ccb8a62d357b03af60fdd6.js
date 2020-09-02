8d1fd3a5084736f919a6a547be3602fe684e4be0
dekim1028 <daeun235194@gmail.com>
2020-09-02 21:47:01 +0900

    Updates

---
+++ precache-manifest.324d1b8e49ccb8a62d357b03af60fdd6.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50cdcb6244be0aaf32afc0c58e3bdd0",
+    "url": "/Bamin/index.html"
+  },
+  {
+    "revision": "3cdcbc27a51ebca172e3",
+    "url": "/Bamin/static/css/main.4c44ea83.chunk.css"
+  },
+  {
+    "revision": "384adb3d5d57874367ca",
+    "url": "/Bamin/static/js/2.cad27c66.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Bamin/static/js/2.cad27c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cdcbc27a51ebca172e3",
+    "url": "/Bamin/static/js/main.7071938e.chunk.js"
+  },
+  {
+    "revision": "c91e65f80174e987a65b",
+    "url": "/Bamin/static/js/runtime-main.13babfa1.js"
+  },
+  {
+    "revision": "d06aef7ee2c0c0ca2d150afb62ec6338",
+    "url": "/Bamin/static/media/brand_logo1.d06aef7e.jpg"
+  },
+  {
+    "revision": "f58c7002c1f8cba8465dd0de5edac89a",
+    "url": "/Bamin/static/media/brand_logo2.f58c7002.jpg"
+  },
+  {
+    "revision": "1490ede55a768d80c642f88b796c8a6f",
+    "url": "/Bamin/static/media/brand_logo3.1490ede5.jpg"
+  },
+  {
+    "revision": "7acc1b923fcbaf36b1b71451598501ec",
+    "url": "/Bamin/static/media/category1.7acc1b92.png"
+  },
+  {
+    "revision": "a29dc26ecf4338415457067098cd28c0",
+    "url": "/Bamin/static/media/category10.a29dc26e.png"
+  },
+  {
+    "revision": "2783e9b514da8fe40635742db8d24398",
+    "url": "/Bamin/static/media/category11.2783e9b5.png"
+  },
+  {
+    "revision": "6a7ecc45f30d12019bdd7362ef6b2cb8",
+    "url": "/Bamin/static/media/category12.6a7ecc45.png"
+  },
+  {
+    "revision": "d0548939824311d902bffea7c7b60cc6",
+    "url": "/Bamin/static/media/category13.d0548939.png"
+  },
+  {
+    "revision": "b95b78ab3cb09507c6f6568bcd7641b8",
+    "url": "/Bamin/static/media/category14.b95b78ab.png"
+  },
+  {
+    "revision": "40eee107bc5788e2a99ce9733f0673fa",
+    "url": "/Bamin/static/media/category15.40eee107.png"
+  },
+  {
+    "revision": "0add84219630f0520041fe3ebe8df4fa",
+    "url": "/Bamin/static/media/category16.0add8421.png"
+  },
+  {
+    "revision": "32262187be2e0ea236b17d261d6c8673",
+    "url": "/Bamin/static/media/category17.32262187.png"
+  },
+  {
+    "revision": "c80be70020ea627a95fb33ad1d29f2bb",
+    "url": "/Bamin/static/media/category18.c80be700.png"
+  },
+  {
+    "revision": "5ac125b24ea0bf16ef92f33378ef61d9",
+    "url": "/Bamin/static/media/category19.5ac125b2.png"
+  },
+  {
+    "revision": "14d451b59c2c9dee5160316711dacc74",
+    "url": "/Bamin/static/media/category2.14d451b5.png"
+  },
+  {
+    "revision": "5521692c7497b8afb3bdb7fdfc2f315e",
+    "url": "/Bamin/static/media/category20.5521692c.png"
+  },
+  {
+    "revision": "50f451e934dd942bbe14e362cec35898",
+    "url": "/Bamin/static/media/category3.50f451e9.png"
+  },
+  {
+    "revision": "be9648c00d064172a6e574d73eb45516",
+    "url": "/Bamin/static/media/category4.be9648c0.png"
+  },
+  {
+    "revision": "6ccee04c4b68f89ca66b5ac26aff678f",
+    "url": "/Bamin/static/media/category5.6ccee04c.png"
+  },
+  {
+    "revision": "0587276c472b2738c11f04f6bef8b460",
+    "url": "/Bamin/static/media/category6.0587276c.png"
+  },
+  {
+    "revision": "c2921624599d6c27cbc9f022cc7d01ce",
+    "url": "/Bamin/static/media/category7.c2921624.png"
+  },
+  {
+    "revision": "1320369e3d60396615f3267119205ed4",
+    "url": "/Bamin/static/media/category8.1320369e.png"
+  },
+  {
+    "revision": "7204a9b22503b63a6235bd3a6044bd43",
+    "url": "/Bamin/static/media/category9.7204a9b2.png"
+  },
+  {
+    "revision": "34f3f9994bb81eb71005b7c1dd93a9b4",
+    "url": "/Bamin/static/media/notice1.34f3f999.png"
+  },
+  {
+    "revision": "ab38ddc4effbf1914fc6c8047afb9947",
+    "url": "/Bamin/static/media/notice2.ab38ddc4.png"
+  },
+  {
+    "revision": "797ea06c414a5e38f7cbe34655056915",
+    "url": "/Bamin/static/media/notice3.797ea06c.png"
+  }
+]);
